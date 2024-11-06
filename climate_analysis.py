4813f208bcef277dd93673d82237055e42623306
Yasmin Jiwa <yasminjiwa@Yasmins-MacBook-Pro-2.local>
2024-11-06 11:51:31 +0000

    Add rainfall processing placeholder

--- climate_analysis.py
+++ climate_analysis.py
@@ -26,3 +26,5 @@ for line in climate_data:
             kelvin = temp_conversion.fahr_to_kelvin(fahr)
 
             print(str(celsius)+", "+str(kelvin))
+
+# TODO(yjiwa): Add call to process rainfall
